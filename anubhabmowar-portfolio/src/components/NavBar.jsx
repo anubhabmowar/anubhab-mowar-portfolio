d5d15e101be818ecaec3e70859756334d6c7ccf8
anubhabmowar <anubhabmowar@gmail.com>
2024-12-12 10:13:31 +0530

    added project files

---
+++ anubhabmowar-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import {FaLinkedinIn as Falinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+const NavBar = () => {
+  return (
+    <nav className='w-screen h-auto mb-20 flex items-center justify-between py-6 px-4'>
+        <div className='flex items-center justify-center h-[75px] w-[75px]  rounded-full object-fit'>
+            <img src={logo} alt="" className='w-full h-full rounded-full' />
+            </div>
+            <div className='flex items-center gap-4'>
+            <Falinkedin className='w-6 h-6 text-white' />
+            <FaGithub className='w-6 h-6 text-white ' />
+            <FaInstagram className='w-6 h-6 text-white' />
+            </div>
+
+    </nav>
+  )
+}
+
+export default NavBar
---
+++ anubhabmowar-portfolio/src/constants/index.js
@@ -0,0 +1,77 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const EDUCATION_CONTENT =[
+    {
+        year: "2023-2027",
+        degree: "Bachelor of Science(B.S) in Data Science and Application",
+        institution: "Indian Institute of Technology, Madras",    
+    },
+];
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications with engaging user interfaces. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating engaging, efficient and user-friendly web applications.I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  
+  {
+    year: "aug,2023 - sep,2023",
+    role: "web development intern",
+    company: "OctaNet",
+    description: `Designed and developed user interfaces for web applications using html,css,js. Worked closely to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "js","APIs"],
+  },
+  {
+    year: "jul,2023 - aug,2023",
+    role: "python Developer intern",
+    company: "SYNC INTERNS",
+    description: `used python and it's libraries and tools like spacy and tkinter to develop small utility apps and simple chatbots.`,
+    technologies: ["Python", "Spacy", "tkinter", "APIs"],
+  
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: " Iphone-3d-website (React App)",
+    image: project1,
+    description:
+      " a modern landing page using React JS,Three JS,React three fibre along with Tailwind CSS for styling and GSAP for animationsR",
+    technologies: ["React","Three JS","React Three Fiber","GSAP", "Tailwind CSS"],
+  },
+  {
+    title: " Exoape-Clone (React App)",
+    image: project2,
+    description:
+      " a portfolio website using React JS, GSAP, Tailwind CSS, and Framer Motion to implement smooth animations and scroll effects.",
+    technologies: ["React", "GSAP", "Tailwind CSS", "Framer Motion"],
+  },
+  {
+    title: " Movixer (React App)",
+    image: project3,
+    description:
+      " a movie browsing app using Tailwind CSS, Redux, React Player, and the TMDB API to showcase trailers and detailed information on movies, TV shows, and celebrities.",
+    technologies: ["React", "Tailwind CSS", "Redux", "React Player", "TMDB API"],
+  },
+  {
+    title: " OBYS Agency Clone",
+    image: project4,
+    description:
+      " a portfolio website using GSAP, Locomotive JS, and Sherry JS for smooth animations and DOM manipulation",
+    technologies: ["GSAP", "Locomotive JS", "Sherry JS"],
+  },
+];
+
+export const CONTACT = {
+  address: "Malda,West Bengal",
+  phoneNo: "+91 7980293529",
+  email: "anubhabmowar@gmail.com",
+};
